41f7d24d66856da238d576b144fb7b7bf70d9526
Flipapa <roamer2010x@gmail.com>
2021-08-11 16:24:20 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = require('./routes')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
+})
